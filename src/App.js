d32b32c1562cfde894a2714e5ab573223ec0624f
Akashmurugan10 <akashmurugan338@gmail.com>
2023-07-07 14:21:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./components/About";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import Portfolio from "./components/Portfolio";
+import SocialLinks from "./components/Sociallinks";
+import Experience from "./components/Experience";
+import Contact from "./components/Contact";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <div>
+         <NavBar />
+         <Home />
+         <About />
+         <Portfolio />
+         <Experience />
+         <Contact />
+
+         <SocialLinks />
+      </div>
+   );
 }
 
 export default App;
